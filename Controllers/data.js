7f58a1e8121871b3fc5fadbf74dbc9c7b638c730
supriya-5 <supriyaprabu005@gmail.com>
2022-08-02 00:02:53 +0530

    backend topic changes

--- Controllers/data.js
+++ Controllers/data.js
@@ -7,14 +7,14 @@ const control = (req,res) =>{
             "id": 1,
             "title": "Moonlight Tour at Palaces",
             "Image": "https://steady-custard-d83563.netlify.app/static/media/one1.03953ea0fb9059ceaa0b.png",
-            "category": "Home",
+            "category": "Home-1",
             "description":"The Moonlight Tour at Gyeongbokgung Palace is one of the most popular tour programs in South Korea. A royal palace, which is an indispensable destination for Korean tourism, features distinctly different sentiments between day and night. Tourists flock to the palace during the day to experience a tranquil and relaxed atmosphere. When covered with a veil of darkness, the palace boasts of a different mood. The subtle lighting and mysterious acoustic effects add up different charms. As those attractive features are known through social networking services, royal palaces have recently emerged as new night attractions. Among royal palaces that open at night are the four major palaces in Seoul (Gyeongbokgung Palace, Changdeok Palace, Changgyeong Palace, and Deoksugung Palace) and Hwaseong Haenggung Palace in Suwon. Whether tour programs are available depends on each royal palace, but if it is, reservations must be made on the website. "
           },
           {
             "id": 1,
             "title": "South Korea to Concentrate on R&D in 10 Sectors",
             "Image": "https://steady-custard-d83563.netlify.app/static/media/imagestech2.30a2553977fbb7a8bf39.jpg",
-            "category": "Home",
+            "category": "Home-2",
             "description":"At present, the United States is planning to invest US$150 billion for five years in the 10 sectors including autonomous driving, biotech, semiconductor and artificial intelligence. China is also increasing its R&D investment in the seven technologies including brain science and artificial intelligence and the eight sectors including smart manufacturing and new material development. Meanwhile, South Korea’s technological level and R&D investment in such sectors are much below those of high-tech industry leaders. In the quantum industry, South Korea’s level is 62.5 percent of that of the global leader whereas those of China and Japan are 93.2 percent and 90.4 percent, respectively. In 2019, South Korea’s R&D investment in the sector stood at 20 trillion won whereas those of the United States, the European Union, China and Japan were 224 trillion won, 124 trillion won, 77 trillion won and 39 trillion won, respectively. The South Korean government’s new goal is to raise those technological levels to at least 90 percent of the highest in the world by 2030. The rest of the 10 are semiconductor and display, cyber security, hydrogen technology, and secondary battery."
           },
          
@@ -22,7 +22,7 @@ const control = (req,res) =>{
             "id": 1,
             "title": "Bibimbap (Rice Bowl)",
             "Image": "https://steady-custard-d83563.netlify.app/static/media/bimbap.4038784e6561c1a0324b.webp",
-            "category": "Home",
+            "category": "Home-3",
             "description":"Bibimbap was created as a Royaldish in yesteryears but slowly it became the favorite staple lunch meal among the Korean dishes."
           },
          
@@ -30,7 +30,7 @@ const control = (req,res) =>{
             "id": 4,
             "title": "BTS",
             "Image": "https://steady-custard-d83563.netlify.app/static/media/BTS.4a7d0e5f729bd6605a83.webp",
-            "category": "Home",
+            "category": "Home-lastest",
             "description":"K-pop superband bts has confirmed that its latest album 'Proof' will stretch across three CDs and consist of 48 tracks."
           },
           
@@ -38,14 +38,14 @@ const control = (req,res) =>{
             "id": 5,
             "title": "The Day I Died",
             "Image": "https://steady-custard-d83563.netlify.app/static/media/sk-movie.3256b80ae567a6d5103b.jpg",
-            "category": "Home",
+            "category": "Home-lastest",
             "description": "Se-jin (Roh Jeong-eui) disappears on a dark and stormy night. Se-jin's death is simply dismissed as a suicide by local detectives. Hyeon-soo has to find the truth behind the suspicious disappearance of the teenage girl."
           },
           {
             "id": 6,
             "title": "Jinhae",
             "Image": "https://steady-custard-d83563.netlify.app/static/media/sk-place.811a2a66acfa41963ff4.jpg",
-            "category": "Home",
+            "category": "Home-lastest",
             "description":"Each spring, the city holds the largest cherry blossom festival in South Korea, with hundreds of thousands of pink trees lining streets, railways, and streams."
           },
          
@@ -53,63 +53,63 @@ const control = (req,res) =>{
           "id": 7,
           "title": "Red rice cakes (tteokbokki) ",
           "Image": "https://steady-custard-d83563.netlify.app/static/media/article-food.1038fc6ef0148621c41f.jpg",
-          "category": "Home",
+          "category": "Home-articles",
           "description": "Tteokbokki is a traditional Korean street food that’s made with thick slices of garaetteok"
         },
         {
           "id": 8,
           "title": "Robots march into more business sectors ",
           "Image": "https://blogs.3ds.com/northamerica/wp-content/uploads/sites/4/2019/08/Robots-Square.jpg",
-          "category": "Home",
+          "category": "Home-articles",
           "description":"The advent of intelligent humanoid robots depicted in sci-fi films is still thought to be a thing of the distant future, but more South Korean companies in a wide range of industries have begun to adopt simpler robots in a bid to remain competitive in the long term."
         },
         {
           "id": 9,
           "title": "Space Sweepers",
           "Image": "https://steady-custard-d83563.netlify.app/static/media/swe.3d9052cfcf7296436948.jpg",
-          "category": "Home",
+          "category": "Home-articles",
           "description":"The story follows the crew of a space junk collector ship called The Victory, as they discover a child humanoid robot and embark on an epic space adventure."
         },
         {
           "id": 10,
           "title": "Blackpink",
           "Image": "https://steady-custard-d83563.netlify.app/static/media/blackpink.4501fda9a25fca165688.webp",
-          "category": "Home",
+          "category": "Home-articles",
           "description":"Blackpink (Korean: 블랙핑크; romanized: Beullaegpingkeu; Japanese: ブラックピンク; romanized: Burakkupinku; commonly stylized as BLɅϽKPIИK) is a South Korean girl group formed by YG Entertainment, consisting of members Jisoo, Jennie, Rosé, and Lisa"
         },
         {
           "id": 11,
           "title": "Jeju Island: A Stunning Island",
           "Image": "https://steady-custard-d83563.netlify.app/static/media/jeju.481dd489936eedb44c2a.webp",
-          "category": "Home",
+          "category": "Home-post",
           "description":"This stunning island just 85 Kilometers off the coast is one of the most beautiful places in South Korea and one of the most famous places in South Korea"
         },
         {
           "id": 12,
           "title": "Train to Busan",
           "Image": "https://steady-custard-d83563.netlify.app/static/media/ttobu.1a4ac713b78645ac84cc.jpg",
-          "category": "Home",
+          "category": "Home-post",
           "description":"Seok-woo and his daughter are on a train to Busan on the latter's birthday to see his wife. However, the journey turns into a nightmare when they are trapped amidst a zombie outbreak in South Korea."
         },
         {
           "id": 13,
           "title": "BTS-Dynamtic",
           "Image": "https://steady-custard-d83563.netlify.app/static/media/bts_main.4418bcb99a83d7a1e687.webp",
-          "category": "Home",
+          "category": "Home-post",
           "description":"'Cause I-I-I'm in the stars tonight So watch me bring the fire and set the night alight.........."
         },
         {
           "id": 14,
           "title": "Samsung",
           "Image":"https://steady-custard-d83563.netlify.app/static/media/Samsung.9773c8fafed267193902.jpg",
-          "category": "Home",
+          "category": "Home-post",
           "description":"MEANING OF THE SAMSUNG LOGO: In Korean, the word Samsung means “three stars.” The name was chosen by Samsung founder Lee Byung-chull"
         },
         {
           "id": 15,
           "title": "Ramen ",
           "Image": "https://steady-custard-d83563.netlify.app/static/media/snacks.5b1111cabdfe7f1333f6.jpg",
-          "category": "Home",
+          "category": "Home-post",
           "description":"ramen in general has been the second most popular Korean food. "
         },
         
