1c73f637b84bb8614ee20170b4a367d12cc6c2d9
supriya-5 <supriyaprabu005@gmail.com>
2022-08-02 13:57:09 +0530

    back

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ const express = require("express")
 
 const app = express()
 const dataRouter = require("./Routes/data")
+const cors = require('cors')
 
 // app.get("/Home", (req,res)=>{
   
 //     res.send("welcome to Home page")
 
 // })
-
+app.use(cors())
 app.use("/api",dataRouter)
 
 app.listen(process.env.PORT||8080, ()=>{
--- Routes/data.js
+++ Routes/data.js
@@ -7,12 +7,7 @@ const dataController = require("../Controllers/data")
  const dataRouter = express.Router()
 //we are linking our data.js file with our /api main
 
-const cors = require("cors");
-const { application } = require("express");
 
-
-
-application.use(cors());
 dataRouter.route("/details")
 
 //creating a route
